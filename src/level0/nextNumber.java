d2d740f89bee2e6c146e90e0fe3dc78b5f74838b
baechaeyoon <baechaeyoon@naver.com>
2023-01-09 00:46:04 +0900

    2nd commit

---
+++ src/level0/nextNumber.java
@@ -0,0 +1,20 @@
+package level0;
+
+public class nextNumber {
+    public static void main(String[] args){
+        int[] list = {1,2,3,4};
+        System.out.println(solution(list));
+    }
+        public static int solution(int[] common) {
+            int answer = 0;
+
+            if(common[1]-common[0]==common[2]-common[1]){
+                answer = common[common.length-1]+(common[1]-common[0]);
+            }
+            else{
+                answer = common[common.length-1]*(common[1]/common[0]);
+            }
+            return answer;
+        }
+
+}
