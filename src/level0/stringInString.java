e7db027645f3079212498c6cb8f25114e054b26e
baechaeyoon <baechaeyoon@naver.com>
2023-01-14 22:46:10 +0900

    update

---
+++ src/level0/stringInString.java
@@ -0,0 +1,20 @@
+package level0;
+
+public class stringInString {
+    public static void main(String[] args){
+        String str1 = "ab6CDE443fgh22iJKlmn1o";
+        String str2 = "6CD";
+        System.out.println(solution(str1,str2));
+    }
+
+    public static int solution(String str1, String str2){
+        int answer = 0;
+        if(str1.contains(str2)){
+            answer = 1;
+        }
+        else{
+            answer = 2;
+        }
+        return answer;
+    }
+}
