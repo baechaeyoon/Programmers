e64ca614a559d055147e8c89dc1d80e1c34c7772
baechaeyoon <baechaeyoon@naver.com>
2023-01-26 20:34:33 +0900

    369게임

---
+++ src/level0/game369.java
@@ -0,0 +1,22 @@
+package level0;
+
+public class game369 {
+
+    public static void main(String[] args){
+        int order = 29423;
+        System.out.println(solution(order));
+    }
+    public static int solution(int order){
+        int answer = 0;
+        int count = 0;
+
+        while(order != 0){
+            if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9){
+                count ++;
+            }
+            order = order/10;
+        }
+        answer = count;
+        return answer;
+    }
+}
